632d54cf11f5167d5b27a1ab772e6f942f9ddc06
Nick <xepecine@gmail.com>
2010-05-05 11:06:54 +0400

    create_inx.py script is ready.

--- create_inx.py
+++ create_inx.py
@@ -2,32 +2,11 @@
 import xml.dom.minidom 
 import getopt, sys, os, re, copy
 
+def usage():
+	print "Usage is not ready yet." # TODO
+
+
 def main():
-	def get_gettext(root_node, current_page="", remove=False):
-		for node in root_node.childNodes :
-			if node.nodeType != node.TEXT_NODE :
-				print(node.nodeType)
-				if node.nodeType != node.ENTITY_NODE:
-					print(node.tagName)
-				get_gettext(node)
-	f = open("gcodetools-dev.inx","r")
-	s = f.read()
-	f.close()
-	
-	s = re.sub( r"(?i).*<!--\s*Gcodetools\s*:\s*(/?)(.*?)\s*-->",r"<\1gcodetools_\2>",s)
-	root = xml.dom.minidom.parseString(s)
-	
-	tags = {}	
-		
-	def recursive(root):
-		for i in root.childNodes:
-			if i.nodeType != xml.dom.minidom.Node.TEXT_NODE :
-				r = re.match(r"gcodetools_(.*)",i.tagName) 	
-				if r :
-					tags[r.group(1)] = i
-				recursive(i)
-#		print tags	
-	recursive(root)
 
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "hi:", ["help", "input=" ])
@@ -36,8 +15,7 @@ def main():
 		print str(err) # will print something like "option -a not recognized"
 		usage()
 		sys.exit(2)
-	output = None
-	verbose = False
+	input_file = None
 	for o, a in opts:
 		if o in ("-h", "--help"):
 			usage()
@@ -47,7 +25,47 @@ def main():
 		else:
 			assert False, "unhandled option"
 
+	if args == []: 
+		args = ["Cutting: ptg area", "ptg area",  "Engraving: engraving", "Tools library: tools_library no_options no_preferences", "Orientation points: orientation no_options no_preferences", "All in one: ptg area engraving tools_library orientation"]
+	
+	f = open("gcodetools-dev.inx" if input_file == None else input_file,"r")
+	s = f.read()
+	f.close()
+	
+	tags = dict(re.findall( r"(?ims)<!--\s*Gcodetools\s*:\s*(.*?)\s*block\s*-->(.*)<!--\s*Gcodetools\s*:\s*/\1\s*block\s*-->",s))
+	
 	for arg_ in args:
+		print "Computing set: %s..." % arg_
+		r = re.match("((.*):)?(.*)",arg_)
+		if r!=None:
+			arg = r.group(3).split()
+			res = '<?xml version="1.0" encoding="UTF-8"?>\n'+tags['header']
+			name = r.group(2) if r.group(2)!=None else r.group(3)
+			id = re.sub("\s|[\.,!@#$%^&*]", "_", r.group(3).lower())
+			res = re.sub("(?ims)<!--\s*Gcodetools\s*:\s*name\s*-->(.*)<!--\s*Gcodetools\s*:\s*/name\s*-->","<name>%s</name>"%name,res)
+			res = re.sub("(?ims)<!--\s*Gcodetools\s*:\s*id\s*-->(.*)<!--\s*Gcodetools\s*:\s*/id\s*-->","<id>ru.cnc-club.filter.gcodetools_%s</id>"%id,res)
+
+			if 'help' not in arg and 'no_help' not in arg : res += tags['help']
+			if 'options' not in arg and 'no_options' not in arg: res += tags['options']
+			if 'preferences' not in arg and 'no_preferences' not in arg : res += tags['preferences']
+
+			for i in arg:
+				if i not in tags and not re.match("no_",i): 
+					print "Can not find tag %s. Ignoring this set %s!\n" % (i,arg_)
+					break
+				if not re.match("no_",i): res += tags[i]
+			if i not in tags and not re.match("no_",i) : continue				
+			submenu ="""		<effects-menu>
+			<submenu _name="Gcodetools"/>
+		</effects-menu>"""
+			res += re.sub("(?ims)<!--\s*Gcodetools\s*:\s*submenu\s*-->(.*)<!--\s*Gcodetools\s*:\s*/submenu\s*-->",submenu,tags['footer'])
+			
+			f = open("gcodetools_%s.inx"%re.sub("\s|[\.,!@#$%^&*]", "_", name.lower()),"w")
+			f.write(res)
+			f.close()
+			print "Done\n"
+					
+		"""		
 		arg = arg_.split()
 		for i in arg:
 			if i not in tags : 
@@ -61,9 +79,8 @@ def main():
 		recursive(inx)
 		for i in tags: 
 			if i not in ["notebook","help","options","id"] + arg:
-				inx.
-			
-		
+				pass			
+		"""
 if __name__ == "__main__":
 	main()
 
